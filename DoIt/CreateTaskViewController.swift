81ba0801db984a6b6107887340d393d4522b79d6
John Nyberg <nybergjd@gmail.com>
2017-07-12 19:45:06 -0400

    End of Lecture 63. Bridged VCs

--- DoIt/CreateTaskViewController.swift
+++ DoIt/CreateTaskViewController.swift
@@ -13,6 +13,8 @@ class CreateTaskViewController: UIViewController {
     @IBOutlet weak var tasknameTextField: UITextField!
     @IBOutlet weak var importantSwitch: UISwitch!
     
+    var previousVC = TasksViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,8 +22,17 @@ class CreateTaskViewController: UIViewController {
     }
     
     @IBAction func addTapped(_ sender: Any) {
+        //Create a task from the outlet information
         
+        let task = Task()
+        task.name = tasknameTextField.text!
+        task.important = importantSwitch.isOn
         
+        //Add new task to array in previous viewcontroller
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
         
     }
 
