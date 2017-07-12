3efadb1fb180380a7ced90f45f6f54401db4ea67
John Nyberg <nybergjd@gmail.com>
2017-07-12 18:57:38 -0400

    End of Lecture 59

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by John Nyberg on 7/12/17.
+//  Copyright © 2017 JohnnyAPPleseed. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = "Unknown"
+    var important = false
+    
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -12,22 +12,49 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tasks = makeTasks()
         
         tableView.dataSource = self
         tableView.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Chocolate Thunder"
+        let task = tasks[indexPath.row]
+        
+        if task.important == true {
+            cell.textLabel?.text = "❗️ \(task.name)"
+        } else{
+        cell.textLabel?.text = task.name
+        }
         return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        
+        let task1 = Task()
+        task1.name = "Walk the Dog"
+        task1.important = true
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = false
+        
+        let task3 = Task()
+        task3.name = "Mow Lawn"
+        task3.important = true
+        
+        return [task1,task2,task3]
+        
     }
     
 }
