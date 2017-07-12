2ae7dc09ea030eb411414540bf6240a2f033f412
John Nyberg <nybergjd@gmail.com>
2017-07-12 19:20:28 -0400

    End of Lecture 61. Added Add View Controller and labels

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by John Nyberg on 7/12/17.
+//  Copyright © 2017 JohnnyAPPleseed. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var tasknameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+        
+        
+    }
+
+ 
+
+}
